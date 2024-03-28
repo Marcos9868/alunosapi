2fdc67819dcddac5c71de603a2629fb95587b4c6
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-03-28 19:35:48 -0300

    Feat: adiciona override que caso não tenha dados na tabela, ele adicione o mock

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,7 +11,27 @@ namespace AlunosApi.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {}
-
         public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Marcos",
+                    Email = "marcos@teste.com",
+                    Idade = 30
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Jess",
+                    Email = "jess@teste.com",
+                    Idade = 21
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
